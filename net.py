3e7614bdc2e7ebf7514cd70c4bab82aee267f220
xryash <demospro1996@gmail.com>
2019-05-25 14:19:45 +0300

    networks options updated

--- net.py
+++ net.py
@@ -2,45 +2,35 @@ import tensorflow as tf
 
 
 def get_networks_options():
+    """Get hyperparams for each neural network"""
     return (
-        {'number': 0, 'epochs': 175, 'learning_rate': 0.0000035, 'dropout_rate': 0.5, 'skip': False},
-        {'number': 1, 'epochs': 150, 'learning_rate': 0.0000035, 'dropout_rate': 0.5, 'skip': False},
-        {'number': 2, 'epochs': 200, 'learning_rate': 0.0000035, 'dropout_rate': 0.5, 'skip': False},
-        {'number': 3, 'epochs': 250, 'learning_rate': 0.0000035, 'dropout_rate': 0.5, 'skip': False},
-
+        {'number': 0, 'epochs': 30, 'learning_rate': 0.0001, 'dropout_rate': 0.3, 'skip': False},
+        {'number': 1, 'epochs': 25, 'learning_rate': 0.0001, 'dropout_rate': 0.3, 'skip': False},
+        {'number': 2, 'epochs': 20, 'learning_rate': 0.0001, 'dropout_rate': 0.3, 'skip': False},
+        {'number': 3, 'epochs': 30, 'learning_rate': 0.0001, 'dropout_rate': 0.3, 'skip': False},
+        {'number': 4, 'epochs': 25, 'learning_rate': 0.0001, 'dropout_rate': 0.3, 'skip': False},
     )
 
 
-def net_arch(x, dropout_rate):
-    initializer = tf.contrib.layers.xavier_initializer()
-    net = tf.layers.batch_normalization(x, axis=1)
-    net = tf.layers.dense(net, 4096, activation=tf.nn.relu, kernel_initializer=initializer)
-    net = tf.layers.batch_normalization(net, axis=1)
-    net = tf.layers.dropout(net, dropout_rate)
-    net = tf.layers.dense(net, 2048, activation=tf.nn.relu, kernel_initializer=initializer)
-    net = tf.layers.batch_normalization(net, axis=1)
-    net = tf.layers.dropout(net, dropout_rate)
-    net = tf.layers.dense(net, 1, activation=None, name='output')
-
-    return net
-
-
 def ensemble_arch(x, dropout_rate):
+    """Neural network ensemble tensorflow graph"""
+
     net = tf.layers.batch_normalization(x)
     initializer = tf.contrib.layers.xavier_initializer()
 
     outs = []
 
+    # compute branch tensors and add to an array
     for i in range(3):
         branch = tf.layers.dense(net, 2048, activation=tf.nn.relu, kernel_initializer=initializer)
         branch = tf.layers.batch_normalization(branch)
         branch = tf.layers.dropout(branch, dropout_rate)
-
         branch = tf.layers.dense(branch, 1024, activation=tf.nn.relu, kernel_initializer=initializer)
         branch = tf.layers.batch_normalization(branch)
 
         outs.append(branch)
 
+    # merge all branch tensors
     net = tf.keras.layers.concatenate(outs)
 
     net = tf.layers.batch_normalization(net)
@@ -49,20 +39,3 @@ def ensemble_arch(x, dropout_rate):
     net = tf.layers.dense(net, 1, activation=None, name='output')
 
     return net
-
-
-"""
-
-    net = tf.layers.dense(x,  4096, activation=tf.nn.relu, kernel_initializer=initializer)
-    net = tf.layers.batch_normalization(net)
-    net = tf.layers.dropout(net, dropout_rate)
-    net = tf.layers.dense(net,  2048, activation=tf.nn.relu, kernel_initializer=initializer)
-    net = tf.layers.batch_normalization(net)
-    net = tf.layers.dropout(net, dropout_rate)
-    net = tf.layers.dense(net,  1024, activation=tf.nn.relu, kernel_initializer=initializer)
-    net = tf.layers.batch_normalization(net)
-    net = tf.layers.dropout(net, dropout_rate)
-    net = tf.layers.dense(net, 1, activation=None)
-"""
-
-
